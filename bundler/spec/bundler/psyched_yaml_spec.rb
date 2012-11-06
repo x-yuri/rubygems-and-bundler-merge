x-yuri/rubygems-and-bundler-merge

commit 452bf716ad781dc5e7a32dddf3dcbb90a0a77db8

we should be rescuing from Psych::SyntaxError, not SyntaxError

bundler/lib/bundler.rb
@@ -293,7 +293,7 @@ module Bundler
         begin
           Gem::Specification.from_yaml(contents)
           # Raises ArgumentError if the file is not valid YAML
-        rescue ArgumentError, SyntaxError, Gem::EndOfYAMLException, Gem::Exception
+        rescue ArgumentError, Psych::SyntaxError, Gem::EndOfYAMLException, Gem::Exception
           begin
             eval(contents, TOPLEVEL_BINDING, path.expand_path.to_s)
           rescue LoadError, SyntaxError => e

commit 8602b6e7048c77ee9c3e9d4eca87524d88250b5f

Fix for Ruby 1.8.7
Commit 452bf716ad781dc5e7a32dddf3dcbb90a0a77db8 introduced a regression.
Psych::SyntaxError is undefined, if 'psych' is not available (e.g. on
Ruby 1.8.7).

bundler/lib/bundler.rb
@@ -293,7 +293,7 @@ module Bundler
         begin
           Gem::Specification.from_yaml(contents)
           # Raises ArgumentError if the file is not valid YAML
-        rescue ArgumentError, Psych::SyntaxError, Gem::EndOfYAMLException, Gem::Exception
+        rescue ArgumentError, YamlSyntaxError, Gem::EndOfYAMLException, Gem::Exception
           begin
             eval(contents, TOPLEVEL_BINDING, path.expand_path.to_s)
           rescue LoadError, SyntaxError => e

bundler/lib/bundler/psyched_yaml.rb
@@ -13,3 +13,9 @@ end
 
 # Psych might NOT EXIST AT ALL
 require 'yaml'
+
+begin
+  YamlSyntaxError = Psych::SyntaxError
+rescue NameError
+  YamlSyntaxError = ArgumentError
+end

bundler/spec/bundler/psyched_yaml_spec.rb
@@ -1,0 +1,7 @@
+require 'spec_helper'
+
+describe YamlSyntaxError do
+  it "is raised on YAML parse errors" do
+    expect{ YAML.parse "{foo" }.to raise_error(YamlSyntaxError)
+  end
+end