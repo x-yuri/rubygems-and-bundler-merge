kelvin211111/bundler

[bundle] Automatically trampoline to postit
Except on bundle exec, since that would be too slow

bundler.gemspec
@@ -24,6 +24,8 @@ Gem::Specification.new do |s|
   s.add_development_dependency "ronn",       "~> 0.7.3"
   s.add_development_dependency "rspec",      "~> 3.0"
 
+  s.dependency                 "postit",     "~> 0.1.1"
+
   s.files = `git ls-files -z`.split("\x0").reject {|f| f.match(%r{^(test|spec|features)/}) }
   # we don't check in man pages, but we need to ship them because
   # we use them to generate the long-form help for each command.

x-yuri/rubygems-and-bundler-merge

bundler/bundler.gemspec
@@ -24,6 +24,8 @@ Gem::Specification.new do |s|
   s.add_development_dependency "ronn",       "~> 0.7.3"
   s.add_development_dependency "rspec",      "~> 3.0"
 
+  s.add_runtime_dependency     "postit",     "~> 0.1.1"
+
   s.files = `git ls-files -z`.split("\x0").reject {|f| f.match(%r{^(test|spec|features)/}) }
   # we don't check in man pages, but we need to ship them because
   # we use them to generate the long-form help for each command.