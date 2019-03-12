komeiatecnologia/bundler

Allow a scape valve to keep the old behavior for now

lib/bundler/settings.rb
@@ -114,13 +114,19 @@ module Bundler
           "bundle config #{key} #{Array(value).join(":")}"
         end
 
-        Bundler::SharedHelpers.major_deprecation 2,\
-          "flags passed to commands " \
-          "will no longer be automatically remembered. Instead please set flags " \
-          "you want remembered between commands using `bundle config " \
-          "<setting name> <setting value>`, i.e. `#{command}`. Once you are " \
-          "ready for the new behavior, use `bundle config set forget_cli_options " \
-          "true` to get rid of this message"
+        unless Bundler.settings[:forget_cli_options] == false
+          Bundler::SharedHelpers.major_deprecation 2,\
+            "flags passed to commands " \
+            "will no longer be automatically remembered. Instead please set flags " \
+            "you want remembered between commands using `bundle config " \
+            "<setting name> <setting value>`, i.e. `#{command}`. Once you are " \
+            "ready for the new behavior, use `bundle config set forget_cli_options " \
+            "true` to get rid of this message. Or if you just want to get rid " \
+            "of this message and stick with the old behavior for now, run " \
+            "`bundle config set forget_cli_options false`, but keep in mind " \
+            "that this behavior will be fully removed in future versions of " \
+            "bundler."
+        end
 
         set_local(key, value)
       end

x-yuri/rubygems-and-bundler-merge

bundler/lib/bundler/settings.rb
@@ -116,13 +116,19 @@ module Bundler
           "bundle config set #{key} #{Array(value).join(":")}"
         end
 
-        Bundler::SharedHelpers.major_deprecation 2,\
-          "flags passed to commands " \
-          "will no longer be automatically remembered. Instead please set flags " \
-          "you want remembered between commands using `bundle config set " \
-          "<setting name> <setting value>`, i.e. `#{command}`. Once you are " \
-          "ready for the new behavior, use `bundle config set forget_cli_options " \
-          "true` to get rid of this message"
+        unless Bundler.settings[:forget_cli_options] == false
+          Bundler::SharedHelpers.major_deprecation 2,\
+            "flags passed to commands " \
+            "will no longer be automatically remembered. Instead please set flags " \
+            "you want remembered between commands using `bundle config set " \
+            "<setting name> <setting value>`, i.e. `#{command}`. Once you are " \
+            "ready for the new behavior, use `bundle config set forget_cli_options " \
+            "true` to get rid of this message. Or if you want to get rid of " \
+            "this message and stick with the old behavior for now, run " \
+            "`bundle config set forget_cli_options false`, but keep in mind " \
+            "that this behavior will be fully removed in future versions of " \
+            "bundler."
+        end
 
         set_local(key, value)
       end