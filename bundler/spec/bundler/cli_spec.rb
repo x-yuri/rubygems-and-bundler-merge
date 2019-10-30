dearshrewdwit/bundler

Fix help with command aliases
When using the `-h` flag with command aliases, bundler was showing a
default help page instead of our manual, so that for example, `bundle
help install` and `bundle install --help` output would be inconstent
with `bundle i --help`.

This commit fixes that.

spec/bundler/cli_spec.rb
@@ -27,6 +27,56 @@ RSpec.describe "bundle executable" do
     expect(out).to eq("Hello, world")
   end
 
+  describe "aliases" do
+    it "aliases e to exec" do
+      bundle "e --help"
+
+      expect(out).to include("BUNDLE-EXEC")
+    end
+
+    it "aliases ex to exec" do
+      bundle "ex --help"
+
+      expect(out).to include("BUNDLE-EXEC")
+    end
+
+    it "aliases exe to exec" do
+      bundle "exe --help"
+
+      expect(out).to include("BUNDLE-EXEC")
+    end
+
+    it "aliases c to check" do
+      bundle "c --help"
+
+      expect(out).to include("BUNDLE-CHECK")
+    end
+
+    it "aliases i to install" do
+      bundle "i --help"
+
+      expect(out).to include("BUNDLE-INSTALL")
+    end
+
+    it "aliases ls to list" do
+      bundle "ls --help"
+
+      expect(out).to include("BUNDLE-LIST")
+    end
+
+    it "aliases cache to package" do
+      bundle "cache --help"
+
+      expect(out).to include("BUNDLE-PACKAGE")
+    end
+
+    it "aliases pack to package" do
+      bundle "pack --help"
+
+      expect(out).to include("BUNDLE-PACKAGE")
+    end
+  end
+
   context "with no arguments" do
     it "prints a concise help message", :bundler => "3" do
       bundle! ""

x-yuri/rubygems-and-bundler-merge

bundler/spec/bundler/cli_spec.rb
@@ -27,6 +27,56 @@ RSpec.describe "bundle executable" do
     expect(out).to eq("Hello, world")
   end
 
+  describe "aliases" do
+    it "aliases e to exec" do
+      bundle "e --help"
+
+      expect(out).to include("BUNDLE-EXEC")
+    end
+
+    it "aliases ex to exec" do
+      bundle "ex --help"
+
+      expect(out).to include("BUNDLE-EXEC")
+    end
+
+    it "aliases exe to exec" do
+      bundle "exe --help"
+
+      expect(out).to include("BUNDLE-EXEC")
+    end
+
+    it "aliases c to check" do
+      bundle "c --help"
+
+      expect(out).to include("BUNDLE-CHECK")
+    end
+
+    it "aliases i to install" do
+      bundle "i --help"
+
+      expect(out).to include("BUNDLE-INSTALL")
+    end
+
+    it "aliases ls to list" do
+      bundle "ls --help"
+
+      expect(out).to include("BUNDLE-LIST")
+    end
+
+    it "aliases package to cache" do
+      bundle "package --help"
+
+      expect(out).to include("BUNDLE-CACHE")
+    end
+
+    it "aliases pack to cache" do
+      bundle "pack --help"
+
+      expect(out).to include("BUNDLE-CACHE")
+    end
+  end
+
   context "with no arguments" do
     it "prints a concise help message", :bundler => "3" do
       bundle! ""